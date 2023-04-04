1c8e1ee499b8ac5e7b8ada705f86c2625fe8a0dd
James <delemar@hotmail.com>
2023-04-03 21:00:14 -0400

    🔧Add script to assist with run/clear all notebooks

---
+++ config.py
@@ -0,0 +1,9 @@
+# Run: jupyter nbconvert --to notebook --inplace --allow-errors --execute --config config.py
+# Clear: jupyter nbconvert --clear-output --to notebook --inplace --config config.py
+# If using docker jupyter notebook prefix with: docker run --rm -v $PWD:/opt/app/data --workdir /opt/app/data jdelemar/jupyterlab-dotnet:6.0.202
+
+c = get_config()
+c.NbConvertApp.notebooks = [
+  "CSharp/Filter, map, reduce - Where, Select, Aggregate.ipynb",
+  "JavaScript/Maintainable JavaScript with Functional Patterns.ipynb",
+]
